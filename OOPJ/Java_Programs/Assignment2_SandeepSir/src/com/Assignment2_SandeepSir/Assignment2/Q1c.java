a473cc31a8b4c0c164e15cf61717ad2edc01e2f2
Amruta Jadhav <amrutaj67@gmail.com>
2024-09-06 07:56:27 +0530

    ASS2(Sanddep Sir) added

---
+++ OOPJ/Java_Programs/Assignment2_SandeepSir/src/com/Assignment2_SandeepSir/Assignment2/Q1c.java
@@ -0,0 +1,11 @@
+package com.Assignment2_SandeepSir.Assignment2;
+
+public class Q1c {
+
+	public static void main(String[] args) {
+		String strStatus = "true";
+		boolean b1 = Boolean.parseBoolean(strStatus);
+		System.out.println(strStatus);
+	}
+
+}
---
+++ OOPJ/Java_Programs/Assignment2_SandeepSir/src/com/Assignment2_SandeepSir/Assignment2/Q1e.java
@@ -0,0 +1,9 @@
+package com.Assignment2_SandeepSir.Assignment2;
+
+public class Q1e {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
