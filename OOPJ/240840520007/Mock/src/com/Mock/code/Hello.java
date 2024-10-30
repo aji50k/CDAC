5b291a3fc702919dde7885489b2ca481c5f890c3
Amruta Sawant <amrutaj67@gmail.com>
2024-10-30 11:33:24 +0530

    updates

---
+++ OOPJ/240840520007/Mock/src/com/Mock/code/Hello.java
@@ -0,0 +1,21 @@
+package com.Mock.code;
+
+public class Hello {
+	static int a;
+	static int b;
+	static String c;
+	
+	Hello(int a, int b, String c){
+		System.out.println("Hello");
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hello h = new Hello(10,20,"Hii");
+		Hello h1 = new Hello();
+		System.out.println();
+	
+
+	}
+
+}
